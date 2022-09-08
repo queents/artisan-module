91e53fef352a5d66ede282df0fae992e1b145e8d
3x1 <EngFadyMondy@gmail.com>
2022-09-08 14:38:06 +0200

    change layout

--- Resources/views/Artisan.vue
+++ Resources/views/Artisan.vue
@@ -8,20 +8,18 @@
 
 <script>
 import { defineComponent } from "vue";
-import AppLayout from "@/Layouts/AppLayout.vue";
-import JetDialogModal from "@/Jetstream/DialogModal.vue";
-import JetSecondaryButton from "@/Jetstream/SecondaryButton.vue";
-import JetButton from "@/Jetstream/Button.vue";
-import JetInput from "@/Jetstream/Input.vue";
-import JetInputError from "@/Jetstream/InputError.vue";
-import JetLabel from "@/Jetstream/Label.vue";
+import JetDialogModal from "@@/Jetstream/DialogModal.vue";
+import JetSecondaryButton from "@@/Jetstream/SecondaryButton.vue";
+import JetButton from "@@/Jetstream/Button.vue";
+import JetInput from "@@/Jetstream/Input.vue";
+import JetInputError from "@@/Jetstream/InputError.vue";
+import JetLabel from "@@/Jetstream/Label.vue";
 import Commands from "./Commands/App.vue";
-import ResourceTableLayout from "@@/Layouts/ResourceTableLayout.vue";
+import AppLayout from "@@/Layouts/AppLayout.vue";
 
 export default defineComponent({
-    layout: ResourceTableLayout,
+    layout: AppLayout,
     components: {
-        AppLayout,
         JetDialogModal,
         JetSecondaryButton,
         JetButton,
